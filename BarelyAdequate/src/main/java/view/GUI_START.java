c820fbb5337e32c7c4536b44626fafc532b38457
teaminadequate <49771079+teaminadequate@users.noreply.github.com>
2019-05-26 16:22:00 -0700

    Add files via upload

--- BarelyAdequate/src/main/java/view/GUI_START.java
+++ BarelyAdequate/src/main/java/view/GUI_START.java
@@ -13,7 +13,6 @@ import model.User;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import java.awt.Font;
@@ -23,6 +22,13 @@ import javax.swing.JScrollPane;
 
 import java.awt.Color;
 import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.JToolBar;
+import java.awt.Button;
+import javax.swing.JMenuItem;
+import javax.swing.border.CompoundBorder;
+import javax.swing.UIManager;
+import javax.swing.ScrollPaneConstants;
 
 public class GUI_START {
 
@@ -147,12 +153,7 @@ public class GUI_START {
 		JButton logInButton = new JButton("Log In");
 		logInButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					loadedUser = new User(nameField.getText(), emailField.getText());
-				} catch (ClassNotFoundException | SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				loadedUser = new User(nameField.getText(), emailField.getText());
 				
 			}
 		});
@@ -168,94 +169,6 @@ public class GUI_START {
 		bgrdHome.setBounds(0, -12, 479, 346);
 		
 		panel.add(bgrdHome);
-		
-//-----------------------MY PROJECTS TAB----------------------------------
-		
-		// Panel for my project tabs
-		JPanel myProjectsPanel = new JPanel(null);
-		
-		tabbedPane.addTab("My Projects", null, myProjectsPanel, null);
-
-		// New project button
-		JButton newButton = new JButton("New Project");
-		newButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//showHiddenProjectFields(true);
-			}
-		});
-		newButton.setForeground(new Color(0, 0, 0));
-		newButton.setBounds(10, 20, 150, 30);
-		// Load project button
-		JButton ldButton = new JButton("Load Project...");
-		ldButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// query database to populate dropdown menu
-				// showHiddenProjectFields(true);
-				// else
-			}
-		});
-		ldButton.setForeground(new Color(0, 0, 0));
-		ldButton.setBounds(10, 55, 150, 30);
-		// save project button
-		JButton svButton = new JButton("Save Project");
-		svButton.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		svButton.setBounds(10, 90, 150, 30);
-		svButton.setForeground(new Color(0, 0, 0));
-		
-		myProjectsPanel.add(newButton);
-		myProjectsPanel.add(ldButton);
-		myProjectsPanel.add(svButton);
-		
-		newButton.setVisible(true);
-		ldButton.setVisible(true);
-		
-		JLabel lblTitle = new JLabel("Title");
-		lblTitle.setBounds(180, 20, 150, 10);
-		JTextArea titleField = new JTextArea();
-		titleField.setBounds(180, 30, 150, 20);
-		
-		JLabel lblPreBill = new JLabel("Current Bill");
-		lblPreBill.setBounds(180, 55, 150, 10);
-		JTextArea preBillField = new JTextArea();
-		preBillField.setBounds(180, 65, 150, 20);
-		JLabel lblPostBill = new JLabel("Projected New Bill");
-		lblPostBill.setBounds(180, 90, 150, 20);
-		JTextArea postBillField = new JTextArea();
-		postBillField.setBounds(180, 105, 150, 20);
-		
-		myProjectsPanel.add(titleField);
-		myProjectsPanel.add(lblTitle);
-		myProjectsPanel.add(preBillField);
-		myProjectsPanel.add(lblPreBill);
-		myProjectsPanel.add(lblPostBill);
-		myProjectsPanel.add(postBillField);
-		
-		JLabel lblProcedure = new JLabel("Procedure");
-		lblProcedure.setBounds(10, 150, 325, 10);
-		JScrollPane procedure = new JScrollPane();
-		procedure.setBounds(10, 165, 325, 150);
-		myProjectsPanel.add(lblProcedure);
-		myProjectsPanel.add(procedure);
-		
-		JLabel lblMaterials = new JLabel("Materials");
-		lblMaterials.setBounds(340, 20, 150, 10);
-		JScrollPane materials = new JScrollPane();
-		materials.setBounds(340, 35, 130, 250);
-		
-		myProjectsPanel.add(lblMaterials);
-		myProjectsPanel.add(materials);
-		
-		JLabel lblDiff = new JLabel("Difficulty");
-		lblDiff.setBounds(340, 280, 130, 20);
-		JSlider diffSlider = new JSlider(1,3);
-		diffSlider.setBounds(340, 300, 130, 20);
-		myProjectsPanel.add(lblDiff);
-		myProjectsPanel.add(diffSlider);
 		
 		
 		
@@ -639,6 +552,187 @@ public class GUI_START {
 		JPanel panel_3 = new JPanel();
 			
 		tabbedPane.addTab("New tab", null, panel_3, null);
+		
+//-----------------------MY PROJECTS TAB----------------------------------
+		
+		// Panel for my project tabs
+		JPanel myProjectsPanel = new JPanel(null);
+		
+		tabbedPane.addTab("My Projects", null, myProjectsPanel, null);
+				
+				JLabel lblTitle = new JLabel("Title would go here");
+				
+				lblTitle.setForeground(Color.DARK_GRAY);
+				
+				lblTitle.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 15));
+				
+				lblTitle.setHorizontalAlignment(SwingConstants.LEFT);
+				
+				lblTitle.setBounds(50, 22, 223, 36);
+				
+				JTextArea preBillField = new JTextArea();
+				
+				preBillField.setBackground(Color.DARK_GRAY);
+				
+				preBillField.setForeground(Color.WHITE);
+				
+				preBillField.setBounds(50, 117, 189, 20);
+				
+				JLabel lblPostBill = new JLabel("Projected Bill:");
+				
+				lblPostBill.setForeground(Color.BLACK);
+				
+				lblPostBill.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 12));
+				
+				lblPostBill.setBounds(50, 58, 130, 20);
+				
+				JTextArea postBillField = new JTextArea();
+				
+				postBillField.setBackground(Color.DARK_GRAY);
+				
+				postBillField.setForeground(Color.WHITE);
+				
+				postBillField.setBounds(47, 78, 192, 20);
+				
+				myProjectsPanel.add(lblTitle);
+				
+				myProjectsPanel.add(preBillField);
+				
+				myProjectsPanel.add(lblPostBill);
+				
+				myProjectsPanel.add(postBillField);
+				
+				JLabel lblCurrentBill = new JLabel("Current Bill:");
+				
+				lblCurrentBill.setForeground(Color.BLACK);
+				
+				lblCurrentBill.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 12));
+				
+				lblCurrentBill.setBackground(Color.LIGHT_GRAY);
+				
+				lblCurrentBill.setBounds(50, 96, 171, 20);
+				
+				myProjectsPanel.add(lblCurrentBill);
+				
+				JLabel lblProcedure = new JLabel("Tasks:");
+				
+				lblProcedure.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 14));
+				
+				lblProcedure.setBackground(Color.BLACK);
+				
+				lblProcedure.setBounds(47, 189, 226, 10);
+				
+				myProjectsPanel.add(lblProcedure);
+				
+				JLabel lblMaterials = new JLabel("Materials:");
+				
+				lblMaterials.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 14));
+				
+				lblMaterials.setBounds(256, 35, 223, 10);
+				
+				JButton newProjectButton = new JButton("New Project");
+				
+				newProjectButton.setForeground(Color.WHITE);
+				
+				newProjectButton.setBackground(Color.DARK_GRAY);
+				
+				newProjectButton.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 11));
+				
+				newProjectButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				
+				JButton btnLoadProject = new JButton("Load Project");
+				
+				btnLoadProject.addActionListener(new ActionListener() {
+					
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
+				btnLoadProject.setForeground(Color.WHITE);
+				
+				btnLoadProject.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 11));
+				
+				btnLoadProject.setBackground(Color.DARK_GRAY);
+				
+				btnLoadProject.setBounds(145, 0, 165, 23);
+				
+				myProjectsPanel.add(btnLoadProject);
+				
+				JButton btnEditProject = new JButton("Edit Project");
+				
+				btnEditProject.addActionListener(new ActionListener() {
+					
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				
+				JTextArea tasksTextArea = new JTextArea();
+				
+				tasksTextArea.setForeground(Color.WHITE);
+				
+				tasksTextArea.setBackground(Color.DARK_GRAY);
+				
+				tasksTextArea.setBounds(50, 200, 189, 123);
+				
+				myProjectsPanel.add(tasksTextArea);
+				
+				JTextArea materialsTextArea = new JTextArea();
+				
+				materialsTextArea.setForeground(Color.WHITE);
+				
+				materialsTextArea.setBackground(Color.DARK_GRAY);
+				
+				materialsTextArea.setBounds(249, 49, 220, 274);
+				
+				myProjectsPanel.add(materialsTextArea);
+				
+				btnEditProject.setForeground(new Color(255, 255, 255));
+				
+				btnEditProject.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 11));
+				
+				btnEditProject.setBackground(Color.DARK_GRAY);
+				
+				btnEditProject.setBounds(304, 0, 175, 23);
+				myProjectsPanel.add(btnEditProject);
+				
+				newProjectButton.setBounds(0, 0, 152, 23);
+				
+				myProjectsPanel.add(newProjectButton);
+				
+				myProjectsPanel.add(lblMaterials);
+				
+				JLabel lblDiff = new JLabel("Difficulty:");
+				
+				lblDiff.setBackground(Color.LIGHT_GRAY);
+				
+				lblDiff.setFont(new Font("Berlin Sans FB Demi", Font.BOLD | Font.ITALIC, 12));
+				
+				lblDiff.setForeground(Color.BLACK);
+				
+				lblDiff.setBounds(50, 137, 171, 20);
+				
+				JSlider diffSlider = new JSlider(1,3);
+				
+				diffSlider.setForeground(Color.GREEN);
+				
+				diffSlider.setBackground(Color.DARK_GRAY);
+				
+				diffSlider.setBounds(50, 158, 189, 20);
+				
+				myProjectsPanel.add(lblDiff);
+				
+				myProjectsPanel.add(diffSlider);
+				
+				
+				JLabel lblNewLabel_3 = new JLabel("");
+				
+				lblNewLabel_3.setIcon(new ImageIcon(GUI_START.class.getResource("/resources/background.png")));
+				lblNewLabel_3.setBounds(0, 0, 490, 334);
+				myProjectsPanel.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		
